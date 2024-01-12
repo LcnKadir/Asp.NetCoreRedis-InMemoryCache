22183e90ab8fed004927cb352341b08a4d64de14
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-12 16:41:39 +0300

    AddMemoryCache and IMemoryCache
    Programcs. MemoryCache has also been added. In the follow-up, the controller was also included in the IMemoryCache interface, creating the “Products” class. // Programcs. ‘e MemoryCache eklenildi. Takibinde controller da “Products” sınıfı oluşturarak, IMemoryCache interface dâhil edildi.

---
+++ InMemoryApp.Web/Controllers/Products.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class Products : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public Products(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
