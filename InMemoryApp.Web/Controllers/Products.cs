61c74be6747cf0cae8be3d3bca0e0f2792209c58
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-16 14:30:59 +0300

    AbsoluteExpiration and SlidingExpiration
    Get and set operations were performed to check whether the key value is in Memory.  Absolute and Sliding Expiration cache trials took place.// Key değerinin Memory’ de olup olmadığını kontrol etmek amacıyla get ve set işlemleri yapıldı.  Absolute ve Sliding Expiration cache denemeleri gerçekleşti.

--- InMemoryApp.Web/Controllers/Products.cs
+++ InMemoryApp.Web/Controllers/Products.cs
@@ -14,6 +14,38 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
+            //Key değerinin Memory'de olup olmadığını tespit etmek için 1.Yol //1 To determine whether the Key value is in Memory or not.The Road//
+
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
+            //Key değerinin Memory'de olup olmadığını tespit etmek için 2.Yol //2 To determine whether the Key value is in Memory or not.The Road//
+            //if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
+
+            //Absolute ve Sliding Expiration' un birlikte kullanımı.////Combined use of Absolute and Sliding Expiration.//
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue<string>("zaman", out string zamancache);
+            ViewBag.zaman = zamancache;
+
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
