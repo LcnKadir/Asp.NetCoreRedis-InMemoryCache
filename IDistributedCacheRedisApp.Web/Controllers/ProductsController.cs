e348bedd5fd57080577a4c99e10181f3d9ce5982
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-18 15:24:04 +0300

    IDistributed cache
    Data saving and deletion were performed via IDistributed cache. // IDistributed cache üzerinden data kaydetme ve silme işlemi uygulandı.

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _distributedCache.SetString("name", "kadir", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("surname", "liçina", cacheEntryOptions);
+            return View();
+        }
+
+        public async Task<IActionResult> Show() 
+        {
+            string name = _distributedCache.GetString("name");
+            ViewBag.name = name;
+            return View(); 
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
+            return View();
+        }
+    }
+}
