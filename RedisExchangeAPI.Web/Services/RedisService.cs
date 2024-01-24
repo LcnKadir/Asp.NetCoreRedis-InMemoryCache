1917d1e238bfdec336cc326335140b5d49d762f0
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-24 13:43:27 +0300

    StackExchange.RedisAPI
    In order to use StackExchange.Redis, API was created and StackExchange package was added. Host and Port connection of Redis was written to the appsetting file. Redis was added to RedisServer created in the Service layer with Configuration operations. The connection of the class was added to the programcs. file. // StachExchange.Redis kullanabilmek adına,  API oluşturuldu ve StackExchange paketi eklendi. Redis’in Host ve Port bağlantısı appsetting dosyasına yazıldı. Redis, Configuration işlemleri ile birlikte Service katmanında oluşturulan RedisServer’a eklendi. Sınıfın bağlantısı programcs. dosyasına eklendi.

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redishost;
+
+        private readonly string _redisport;
+
+        private ConnectionMultiplexer _redis; //connection for redis server.//
+
+        public IDatabase db { get; set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            _redishost = configuration["Redis:Host"];
+            _redisport = configuration["Redis:Port"];
+        }
+
+
+
+
+        //The communication of the redis server, which we specified in appseting, took place with the connetcion method.//
+        public void Connet()
+        {
+            var configserver = $"{_redishost}:{_redisport}";
+
+            _redis = ConnectionMultiplexer.Connect(configserver);
+
+        }
+
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+
+    }
+}
