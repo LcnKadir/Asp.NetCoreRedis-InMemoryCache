db99873beda45d2303bf144eab91e2265d26d4c2
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-26 00:56:39 +0300

    StackExchange.RedisAPI/List
    Controller was created (ListType) and listing was performed with data insertion (ListLeftPush-ListRightPush). ListRightPop and ListLeftPop were used to delete the listed data.  // Controller oluşturuldu(ListType) ve veri ekleme işlemi ile birlikte listelenme gerçekleştirildi(ListLeftPush-ListRightPush). Listelenen verilerin silinmesi için ListRightPop ve ListLeftPop kullanıldı.

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+
+        private string listkey = "names";
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(1);
+        }
+
+        public IActionResult Index()
+        {
+            List<string> namelist = new List<string>();
+
+            if (db.KeyExists(listkey))
+            {
+                db.ListRange(listkey).ToList().ForEach(x =>
+                {
+                    namelist.Add(x.ToString());
+                });
+            }
+            return View(namelist);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            db.ListRightPush(listkey, name);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Delete(string name)
+        {
+            db.ListRemoveAsync(listkey,name).Wait();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFirstList()
+        {
+            db.ListLeftPop(listkey);
+           //db.ListRightPop(listkey);
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
