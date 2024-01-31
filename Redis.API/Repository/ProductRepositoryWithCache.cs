90aa7d4c7540e88c98309b4758874080572f2a92
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-01-31 20:39:17 +0300

    ProductRepositoryWithCache - Decorater Desing Pattern
    With the ProductRepositoryWithCache class, the necessary coding was written to pull the data from the cache and add the data to the cache. // ProductRepositoryWithCache sınıfı ile dataların cache’den çekilmesi ve dataların cache eklenmesi için gerekli kodlamalar yazıldı.

--- Redis.API/Repository/ProductRepositoryWithCache.cs
+++ Redis.API/Repository/ProductRepositoryWithCache.cs
@@ -1,32 +1,77 @@
 ﻿using Redis.API.Models;
 using RedisExampleApp.Cache;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace Redis.API.Repository
 {
     public class ProductRepositoryWithCache : IProductRepository
     {
+        private const string productKey = "productCaches";
         private readonly IProductRepository _Productrepository;
         private readonly RedisService _redisService;
+        private readonly IDatabase _cacherepository;
 
         public ProductRepositoryWithCache(IProductRepository productrepository, RedisService redisService)
         {
             _Productrepository = productrepository;
             _redisService = redisService;
+            _cacherepository = _redisService.Getdb(4);
         }
 
-        public Task<Product> CreateAsync(Product product)
+        public async Task<Product> CreateAsync(Product product)
         {
-            throw new NotImplementedException();
+            var newProduct = await _Productrepository.CreateAsync(product);
+
+            if(await _cacherepository.KeyExistsAsync(productKey)) 
+            {
+                await _cacherepository.HashSetAsync(productKey, product.Id, JsonSerializer.Serialize(newProduct));
+            }
+
+            return newProduct;
         }
 
-        public Task<List<Product>> GetAsync()
+        public async Task<List<Product>> GetAsync()
         {
-            throw new NotImplementedException();
+            if (!await _cacherepository.KeyExistsAsync(productKey))
+                return await LoadToCacheFromDbAsync();
+
+            var products = new List<Product>();
+
+            var cacheProducts = await _cacherepository.HashGetAllAsync(productKey);
+            foreach (var item in cacheProducts.ToList())
+            {
+                var product = JsonSerializer.Deserialize<Product>(item.Value);
+                products.Add(product);
+            }
+            return products;
         }
 
-        public Task<Product> GetByIdAsync(int id)
+        public async Task<Product> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            if(_cacherepository.KeyExists(productKey))
+            {
+                var product = await _cacherepository.HashGetAsync(productKey, id);
+                return product.HasValue ? JsonSerializer.Deserialize<Product>(product) : null;
+            }
+
+            var products = await LoadToCacheFromDbAsync();
+            return products.FirstOrDefault(x => x.Id == id);
         }
+
+
+        public async Task<List<Product>> LoadToCacheFromDbAsync()
+        {
+            var product = await _Productrepository.GetAsync();
+
+            product.ForEach(p =>
+            {
+                _cacherepository.HashSetAsync(productKey, p.Id, JsonSerializer.Serialize(p));
+            });
+
+            return product;
+        }
+
+
     }
 }
